44dfec037df952c2f7d4d3d203f98954e98a2ccc
Edward Minaya <edmiya.dev@gmail.com>
2025-01-25 15:02:58 -0400

    the contact form has been composed

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,125 +1,14 @@
-import { useState } from "react";
+import { ContactForm } from "./ContactForm";
 
 export const Contact = () => {
-  const [form, setForm] = useState({
-    name: "",
-    phone: "",
-    email: "",
-    message: "",
-  });
-
-  const onInputChange = ({ target }: any) => {
-    const { name, value } = target;
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
-  const onSubmitForm = (event : React.FormEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    // haga lo que tenga que hacer
-
-    console.log("se envio");
-
-    setForm({
-      name: "",
-      phone: "",
-      email: "",
-      message: "",
-    });
-  };
-
   return (
     <section id="contact" className="mb-28">
-      <h2 className="font-bold text-4xl text-center mb-8 dark:text-white">Contacto</h2>
+      <h2 className="font-bold text-4xl text-center mb-8 dark:text-white">
+        Contacto
+      </h2>
       <div className="flex flex-row container mx-auto">
         <img src="./img/contact.webp" alt="" className="hidden lg:flex w-1/2" />
-
-        <form className="mx-auto w-full px-4 lg:max-w-md lg:w-1/2">
-          <div className="relative z-0 w-full mb-5 group">
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer dark:text-white"
-              placeholder=" "
-              required
-              onChange={onInputChange}
-              value={form.name}
-            />
-            <label
-              htmlFor="floating_email"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Nombre
-            </label>
-          </div>
-
-          <div className="relative z-0 w-full mb-5 group">
-            <input
-              type="tel"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              name="phone"
-              id="phone"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer dark:text-white"
-              placeholder=" "
-              required
-              onChange={onInputChange}
-              value={form.phone}
-            />
-            <label
-              htmlFor="phone"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Telefono (123-456-7890)
-            </label>
-          </div>
-
-          <div className="relative z-0 w-full mb-8 group">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="block py-2.5 mb-4 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer dark:text-white"
-              placeholder=" "
-              onChange={onInputChange}
-              value={form.email}
-            />
-            <label
-              htmlFor="email"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Correo Electronico
-            </label>
-          </div>
-
-          <div className="relative z-0 w-full mb-5 group">
-            <textarea
-              id="message"
-              rows={4}
-              className="block py-2.5 pt-1 px-0  pl-1 w-full text-sm text-gray-900 bg-transparent border-2 rounded-md border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer dark:text-white"
-              placeholder=" "
-              name="message"
-              onChange={onInputChange}
-              value={form.message}
-            ></textarea>
-            <label
-              htmlFor="message"
-              className="peer-focus:font-medium absolute ml-1 text-sm text-gray-500 dark:text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8"
-            >
-              Mensaje
-            </label>
-          </div>
-
-          <button
-            type="submit"
-            onClick={onSubmitForm}
-            className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Submit
-          </button>
-        </form>
+        <ContactForm />
       </div>
     </section>
   );
